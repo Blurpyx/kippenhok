1f013ca58ec5b0224f49dfad48ab7f750d81a484
Blurpyx <tommytytgat.tt@gmail.com>
2022-07-26 11:00:50 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -5,33 +5,83 @@
 #define LDR A0
 #define button 7
 // Stepper
-#define dirPin 1
-#define stepPin 3
+#define dirPin 11
+#define stepPin 12
 #define enPin 5
-#define motorInterfaceType 1
+
+#define isOpen 2
+#define isClosed 3
 
 bool overrideState = false;
 bool lastBtnState = false;
 bool currBtnState = false;
 
-bool gateOpen = false;
+bool opened = false;
 
 int value = 0;
+int stSp = 80;
+int mxSp = stSp * 10;
 
+#define motorInterfaceType 1
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 void setup() {
   Serial.begin(9600);
   pinMode(button, INPUT);
   pinMode(enPin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(isOpen), CloseUp, RISING);
+  attachInterrupt(digitalPinToInterrupt(isClosed), CloseCl, RISING);
+  
+  /*attachInterrupt(digitalPinToInterrupt(isOpen), StepUp, FALLING);
+  attachInterrupt(digitalPinToInterrupt(isClosed), StepCl, FALLING);*/
 
   currBtnState = digitalRead(button);
   
-  stepper.setMaxSpeed(2000);
-  stepper.setAcceleration(10000);
-  digitalWrite(enPin, HIGH);
+  stepper.setMaxSpeed(mxSp);
+  stepper.setSpeed(stSp);
+  digitalWrite(enPin, LOW);
 }
 
+void CloseUp() {
+  if(!opened && digitalRead(isOpen)) {
+    opened = true;
+    stepper.stop();
+  }
+}
+
+void CloseCl() {
+  if(opened && digitalRead(isClosed)) {
+    opened = false;
+    stepper.stop();
+  }
+}
+void OpenGate() {
+  // Open gate
+  stepper.setSpeed(stSp);
+  while(!opened) stepper.runSpeed();
+}
+void CloseGate() {
+  // Close gate
+  stepper.setSpeed(-stSp);
+  while(opened) stepper.runSpeed();
+}
+/*
+void StepUp() {
+  stepper.stop();
+  stepper.setSpeed(stSp);
+  while(!opened) {
+    stepper.runSpeed();
+  }
+}
+
+void StepCl() {
+  stepper.stop();
+  stepper.setSpeed(-stSp);
+  while(opened) {
+    stepper.runSpeed();
+  }
+}
+*/
 void loop() {
   Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();
   Serial.println("----------------------------------------------------------------------------");
@@ -40,8 +90,6 @@ void loop() {
   Serial.println(currBtnState ? "pressed" : "not pressed");
   Serial.print("overrideState ");
   Serial.println(overrideState ? "enabled" : "disabled");
-  Serial.print("gate is ");
-  Serial.println(gateOpen ? "open" : "closed");
   
   if (!overrideState) {
     value = analogRead(LDR);
@@ -55,46 +103,24 @@ void loop() {
       Serial.print("Button Pressed! overrideState ");
       overrideState = !overrideState;
       Serial.println(overrideState ? "enabled" : "disabled");
-      // Toggle Gate
-      if (gateOpen) {
-        digitalWrite(enPin, LOW);
-        stepper.moveTo(0);
-        stepper.runToPosition();
-        digitalWrite(enPin, HIGH);
-        gateOpen = !gateOpen;
-      }
-      else if (!gateOpen) {
-        digitalWrite(enPin, LOW);
-        stepper.moveTo(3000);
-        stepper.runToPosition();
-        digitalWrite(enPin, HIGH);
-        gateOpen = !gateOpen;
-      }
+      // Open gate
+      if(!opened) OpenGate();
+      // Close gate
+      if(opened) CloseGate();
+      
     }
   }
 
   if (!overrideState) {
-    if (value < 200) {
-      // Donker
-      if (gateOpen) {
-        digitalWrite(enPin, LOW);
-        stepper.moveTo(0);
-        stepper.runToPosition();
-        digitalWrite(enPin, HIGH);
-        gateOpen = !gateOpen;
-      }
+    if (!opened && (value < 300)) {
+      // Open gate
+      while(!opened) stepper.setSpeed(stSp); stepper.runSpeed(); 
     }
-    else if (value > 200) {
-      // Licht
-      if (!gateOpen) {
-        digitalWrite(enPin, LOW);
-        stepper.moveTo(3000);
-        stepper.runToPosition();
-        digitalWrite(enPin, HIGH);
-        gateOpen = !gateOpen;
-      }
+    else if (value > 300) {
+      // Close gate
+      while(opened) stepper.setSpeed(-stSp); stepper.runSpeed();
     }
   }
-  
-  delay(500);
+
+  //delay(500);
 }
